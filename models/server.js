fdb333cfbfddae9232c73dd8eab9164624f65be3
Andres <34123576+andres-david@users.noreply.github.com>
2023-08-13 14:36:00 +0200

    Update server.js

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ import { router } from "../routes/user.js";
 export class Server {
   constructor(){
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.usuariosPath = '/api/usuarios';
 
     // Middlewares
